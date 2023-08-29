adfd66396cce4502fe08c52136201332097a8cc8
cjf215 <87613704+cjf215@users.noreply.github.com>
2023-08-29 15:18:17 -0500

    test commit
    testing committing a jupyter notebook

---
+++ test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print(hello world)
+
+
+# In[4]:
+
+
+print("Hello, World!")
+
+
+# In[5]:
+
+
+print("Hello, World!")
+
+
+# In[6]:
+
+
+import os
+print(os.getcwd())
+
+
+# In[8]:
+
+
+os.chdir('/Users/coralflanagan1/Desktop/courses/Gannon-DS/intro-to-github-cjf215')
+
+
+# In[9]:
+
+
+os.chdir('/Users/coralflanagan1/Desktop/courses/Gannon-DS/intro-to-github-cjf215')
+
+
+# In[10]:
+
+
+print(os.getcwd())
+
+
+# In[ ]:
+
+
+
+
